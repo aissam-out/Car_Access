acfdaa2191e6f907c36c9e46f4ade6217090d650
aissam <39061689+aissam-out@users.noreply.github.com>
2020-05-30 03:00:33 +0100

    Add files via upload

---
+++ PIP/API_pip_process.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request
+from utils import *
+import csv
+
+app = Flask(__name__)
+
+@app.route('/pip', methods=['POST'])
+def index():
+    # get concrete entities
+    json_content = request.get_json()
+    org = json_content['org']
+    sub = json_content['subject']
+    obj = json_content['object']
+    act = json_content['action']
+    ctx = json_content['ctx']
+
+    # match concrete and abstract entities
+    role = empower(org, sub)[2]
+    view = use(org, obj)[2]
+    ay = consider(org, act)[2]
+    context = check_context(ctx[0], ctx[1])[2]
+
+    fields = [org, role, view, ay, context]
+
+    with open(r'request.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
+    return str(fields)
+
+@app.route('/pap', methods=['POST'])
+def pap_func():
+    # get abstract entities
+    json_content = request.get_json()
+    org = json_content['org']
+    role = json_content['role']
+    view = json_content['view']
+    activity = json_content['activity']
+    context = json_content['context']
+
+    # get decision from PAP
+    response = check_decision(org, role, view, activity, context)
+
+    with open(r'response.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(response)
+    
+    return str(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
