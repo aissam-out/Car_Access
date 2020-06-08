4a2fa091c3e2251e1dc9b2d6e78c3820929a72f9
aissam <39061689+aissam-out@users.noreply.github.com>
2020-06-08 04:06:10 +0100

    Update get_match.py

--- PDP/get_match.py
+++ PDP/get_match.py
@@ -1,9 +1,11 @@
 import requests, json
 import csv
 
+# the URL of the PIP API
 url = 'http://127.0.0.1:5000/pip'
 #url = 'https://pipcar.herokuapp.com/pip'
 
+# the concrete entities s, o, a, c, as well as org and id
 data = {
 'id':'29',
 'org': 'caraccess',
@@ -13,6 +15,7 @@ data = {
 'ctx': ['2020-08-03', '2020-11-29']
 }
 
+# prepare and post the request to the PIP using http
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 r = requests.post(url, data=json.dumps(data), headers=headers)
