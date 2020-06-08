e0874a9e55d320aca93f5925948ab3004970cf8c
aissam <39061689+aissam-out@users.noreply.github.com>
2020-06-08 11:40:08 +0100

    Update get_access.py

--- request_preparation/get_access.py
+++ request_preparation/get_access.py
@@ -4,7 +4,7 @@ send request to Raspberry via MQTT
 import paho.mqtt.publish as publish
 
 MQTT_SERVER = "192.168.1.63"
-MQTT_PATH = "requests"
+MQTT_PATH = "requests" # topic
 
 def get_axx(fields):
     publish.single(MQTT_PATH, str(fields), hostname=MQTT_SERVER)
