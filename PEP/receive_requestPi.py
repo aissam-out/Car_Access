4ceeb87cb3f3d2459ef3da17466c0cfa02de9afe
aissam <39061689+aissam-out@users.noreply.github.com>
2020-06-08 11:45:59 +0100

    Update receive_requestPi.py

--- PEP/receive_requestPi.py
+++ PEP/receive_requestPi.py
@@ -2,12 +2,14 @@ import paho.mqtt.client as mqtt
 import csv
 
 MQTT_SERVER = "localhost"
-MQTT_PATH = "requests"
+MQTT_PATH = "requests" # topic
 
+# subscribe to the topic requests
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe(MQTT_PATH)
 
+# print & save the received msg
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     with open(r'received.csv', 'a') as f:
@@ -17,6 +19,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
+# connect to the broker
 client.connect(MQTT_SERVER, 1883, 60)
 
 client.loop_forever()
