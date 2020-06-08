14e41d61eac43facdea8c2442bc64b4a51fea64f
aissam <39061689+aissam-out@users.noreply.github.com>
2020-06-08 04:08:16 +0100

    Update get_decision.py

--- PDP/get_decision.py
+++ PDP/get_decision.py
@@ -1,9 +1,11 @@
 import requests, json
 import csv
 
+# the URL of the PAP API
 #url = 'http://127.0.0.1:5000/pap'
 url = 'https://pipcar.herokuapp.com/pap'
 
+# the abstract entities r, v, ay, ctx as well as org
 data = {
 'org':'caraccess',
 'role': 'normal',
@@ -12,6 +14,7 @@ data = {
 'context': 'off'
 }
 
+# prepare and post the request to the PAP using HTTP
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 r = requests.post(url, data=json.dumps(data), headers=headers)
